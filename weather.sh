3adedba2684ef3dabd9f427862c46eb8b11e9ac8
XQ0118 <173141178@qq.com>
2021-03-16 10:57:31 +0800

    feat: init weatheraction

---
+++ weather.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+set -eux
+
+CITY=Hangzhou
+LANGUAGE="zh-CN"
+UNIT=m
+UA="Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.192 Safari/537.36"
+
+curl \
+  -H "Accept-Language: $LANGUAGE" \
+  -H "User-Agent: $UA" \
+  -o result.html \
+  wttr.in/$CITY?format=4\&$UNIT
