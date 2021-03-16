e3a64294b7a06ebb8bae58461e7d9bb536febf2a
XQ0118 <173141178@qq.com>
2021-03-16 11:35:23 +0800

    feat: update weather.sh & add beijing & kunming

---
+++ kunming.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+set -eux
+
+CITY=Kunming
+LANGUAGE="zh-CN"
+UNIT=m
+UA="Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.192 Safari/537.36"
+
+curl \
+  -H "Accept-Language: $LANGUAGE" \
+  -H "User-Agent: $UA" \
+  -o kunminmg.html \
+  wttr.in/$CITY
